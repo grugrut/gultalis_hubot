c87399741596b65e8b39c128f80430bb4de5f72e
grugrut <grugruglut+github@gmail.com>
2017-08-02 23:32:01 +0900

    Generate dummy graph with chartjs-node

--- scripts/influxdb_graph.js
+++ scripts/influxdb_graph.js
@@ -11,22 +11,42 @@
 //   grugrut <grugruglut+github _at_ gmail.com>
 
 Canvas = require('canvas');
+ChartjsNode = require('chartjs-node');
 mktemp = require('mktemp');
 path = require('path');
 fs = require('fs');
 
+chartJsOptions = {
+    type: 'line',
+    data: {
+        labels: ['M', 'T', 'W'],
+        datasets: [{
+            label: 'apples',
+            data: [12, 19, 3],
+            backgroundColor: "rgba(255, 0, 0, 0.5)"
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: false
+                }
+            }]
+        }
+    }
+};
+
 module.exports = function(robot) {
     robot.respond(/graph/i, function(msg) {
-        canvas = new Canvas(200, 200);
-        ctx = canvas.getContext('2d');
-        ctx.fillStyle = 'rgba(0, 0, 255, 0.5)';
-        ctx.fillRect(10, 10, 190, 190);
-        mktemp.createFile(path.join('/tmp', 'XXXXXXXX.png'), function(err, filename) {
-            stream = canvas.createPNGStream();
-            outStream = fs.createWriteStream(filename);
-            stream.on('data', function(chunk) {
-                outStream.write(chunk);
+        msg.send ('Start...');
+        var chartNode = new ChartjsNode(400, 400);
+        chartNode.drawChart(chartJsOptions)
+            .then(() => {
+                return chartNode.getImageStream('image/png');
+            })
+            .then(streamResult => {
+                return chartNode.writeImageToFile('image/png', './testimage.png');
             });
-        });
     });
 };
