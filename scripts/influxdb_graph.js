bb8b1db53cf2a6b1847031c74c8de971aa088733
grugrut <grugruglut+github@gmail.com>
2017-08-03 22:32:32 +0900

    generate performance graph

--- scripts/influxdb_graph.js
+++ scripts/influxdb_graph.js
@@ -10,43 +10,93 @@
 // Author:
 //   grugrut <grugruglut+github _at_ gmail.com>
 
-Canvas = require('canvas');
-ChartjsNode = require('chartjs-node');
-mktemp = require('mktemp');
-path = require('path');
-fs = require('fs');
+var ChartjsNode = require('chartjs-node');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
 
-chartJsOptions = {
-    type: 'line',
-    data: {
-        labels: ['M', 'T', 'W'],
-        datasets: [{
-            label: 'apples',
-            data: [12, 19, 3],
-            backgroundColor: "rgba(255, 0, 0, 0.5)"
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: false
+targetJson = `{
+    "results": [
+        {
+            "statement_id": 0,
+            "series": [
+                {
+                    "name": "mem",
+                    "columns": [
+                        "time",
+                        "used_percent"
+                    ],
+                    "values": [
+                        [
+                            "2017-08-02T12:38:00Z",
+                            80.14317538453697
+                        ],
+                        [
+                            "2017-08-02T12:38:30Z",
+                            80.25594033438736
+                        ],
+                        [
+                            "2017-08-02T12:39:00Z",
+                            80.24192435192009
+                        ]
+                    ]
                 }
-            }]
+            ]
         }
-    }
-};
+    ]
+}`;
 
 module.exports = function(robot) {
     robot.respond(/graph/i, function(msg) {
-        msg.send ('Start...');
-        var chartNode = new ChartjsNode(400, 400);
-        chartNode.drawChart(chartJsOptions)
-            .then(() => {
-                return chartNode.getImageStream('image/png');
-            })
-            .then(streamResult => {
-                return chartNode.writeImageToFile('image/png', './testimage.png');
-            });
+        var chartNode = new ChartjsNode(400, 200);
+
+        request.get({url:'http://localhost:8086/query?pretty=true --data-urlencode "db=monitoring" --data-urlencode "q=select used_percent from mem where time > now() - 24h"'},
+                    function(error, response, body) {
+                        var parsedJson = JSON.parse(targetJson);
+                        var label = [];
+                        var data = [];
+                        for (let value of parsedJson['results'][0]['series'][0]['values']) {
+                            label.push(value[0]);
+                            data.push(value[1]);
+                        }
+                        
+                        chartNode.drawChart( {
+                            type: 'line',
+                            data: {
+                                labels: label,
+                                datasets: [{
+                                    label: parsedJson['results'][0]['series'][0]['name'],
+                                    data: data,
+                                    backgroundColor: "rgba(255, 0, 0, 0.5)"
+                                }]
+                            },
+                            options: {
+                                scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: false
+                                        }
+                                    }]
+                                }
+                            }})
+                            .then(() => {
+                                return chartNode.getImageStream('image/png');
+                            })
+                            .then(streamResult => {
+                                return chartNode.writeImageToFile('image/png', '/tmp/testimage.png');
+                            })
+                            .then(() => {
+                                request.post({url:'https://slack.com/api/files.upload',
+                                              formData: {
+                                                  token: process.env.HUBOT_SLACK_TOKEN,
+                                                  filename: 'testimage.png',
+                                                  file: fs.createReadStream('/tmp/testimage.png'),
+                                                  channels: 'develop'
+                                              }},
+                                             function(error, response, body) {
+                                             });
+                            });
+                        
+                    });
     });
 };
