f668ccf77cc56aa3fb7d1ee032d29f6fa0b485a8
grugrut <grugruglut+github@gmail.com>
2017-08-04 00:17:15 +0900

    refactoring.

--- scripts/influxdb_graph.js
+++ scripts/influxdb_graph.js
@@ -17,60 +17,64 @@ var request = require('request');
 
 module.exports = function(robot) {
     robot.respond(/graph/i, function(msg) {
-        var chartNode = new ChartjsNode(400, 300);
-
-        request.get({url:'http://localhost:8086/query?pretty=true&db=monitoring&q='+encodeURIComponent('select used_percent from mem where time > now() - 24h')},
-                    function(error, response, body) {
-                        if (error) {
-                            console.log(error);
-                            return;
-                        }
-                        var parsedJson = JSON.parse(body);
-                        var label = [];
-                        var data = [];
-                        for (let value of parsedJson['results'][0]['series'][0]['values']) {
-                            var d = new Date(value[0]);
-                            label.push(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
-                            data.push(value[1]);
-                        }
-                        
-                        chartNode.drawChart( {
-                            type: 'line',
-                            data: {
-                                labels: label,
-                                datasets: [{
-                                    label: parsedJson['results'][0]['series'][0]['name'],
-                                    data: data,
-                                    backgroundColor: "rgba(255, 0, 0, 0.5)"
-                                }]
-                            },
-                            options: {
-                                scales: {
-                                    yAxes: [{
-                                        ticks: {
-                                            beginAtZero: false
-                                        }
-                                    }]
-                                }
-                            }})
-                            .then(() => {
-                                return chartNode.getImageStream('image/png');
-                            })
-                            .then(streamResult => {
-                                return chartNode.writeImageToFile('image/png', '/tmp/testimage.png');
-                            })
-                            .then(() => {
-                                request.post({url:'https://slack.com/api/files.upload',
-                                              formData: {
-                                                  token: process.env.HUBOT_SLACK_TOKEN,
-                                                  filename: 'testimage.png',
-                                                  file: fs.createReadStream('/tmp/testimage.png'),
-                                                  channels: 'develop'
-                                              }},
-                                             function(error, response, body) {
-                                             });
-                            });
-                        
-                    });
+        createGraph('used_percent', 'mem');
     });
+};
+
+function createGraph(key, name) {
+    var chartNode = new ChartjsNode(400, 300);
+
+    request.get({url:'http://localhost:8086/query?pretty=true&db=monitoring&q='+encodeURIComponent('select mean(' + key +') from ' + name + ' where time > now() - 24h GROUP BY (10m)')},
+                function(error, response, body) {
+                    if (error) {
+                        console.log(error);
+                        return;
+                    }
+                    var parsedJson = JSON.parse(body);
+                    var label = [];
+                    var data = [];
+                    for (let value of parsedJson['results'][0]['series'][0]['values']) {
+                        var d = new Date(value[0]);
+                        label.push(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
+                        data.push(value[1]);
+                    }
+                    
+                    chartNode.drawChart( {
+                        type: 'line',
+                        data: {
+                            labels: label,
+                            datasets: [{
+                                label: parsedJson['results'][0]['series'][0]['name'],
+                                data: data,
+                                backgroundColor: "rgba(255, 0, 0, 0.5)"
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero: false
+                                    }
+                                }]
+                            }
+                        }})
+                        .then(() => {
+                            return chartNode.getImageStream('image/png');
+                        })
+                        .then(streamResult => {
+                            return chartNode.writeImageToFile('image/png', '/tmp/testimage.png');
+                        })
+                        .then(() => {
+                            request.post({url:'https://slack.com/api/files.upload',
+                                          formData: {
+                                              token: process.env.HUBOT_SLACK_TOKEN,
+                                              filename: 'testimage.png',
+                                              file: fs.createReadStream('/tmp/testimage.png'),
+                                              channels: 'develop'
+                                          }},
+                                         function(error, response, body) {
+                                         });
+                        });
+                    
+                });
 };
